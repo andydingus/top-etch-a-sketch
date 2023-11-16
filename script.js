dd7072c5da1366bb3a884b7b96189b8901f81dff
Odmund Bautista <odfelip.bautista@gmail.com>
2023-11-16 09:37:52 -0600

    Create grid in JS file

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Ready to script!
+const container = document.getElementById('container');
+for (let i = 0; i < 32; i ++) {
+    const div = document.createElement('div');
+    div.className = 'grid';
+    div.textContent = 'pluh';
+    document.body.appendChild(div);
+    container.appendChild(div);
+}
+
+// const div = document.createElement('div');
+
+// document.body.onload()
