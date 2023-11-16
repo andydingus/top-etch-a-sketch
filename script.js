5f4c3b82719e9b377d895dc5f5f73aec0e0e7daa
Odmund Bautista <odfelip.bautista@gmail.com>
2023-11-16 12:12:32 -0600

    Progress on button functionality
    Going to get lunch!

--- script.js
+++ script.js
@@ -6,22 +6,47 @@ let gridBlockHeight = gridBlockWidth; // Should be the same as the width
 let gridBorderWidth = '2px';
 let gridSize = 16;
 let containerWidth = gridBlockWidth * gridSize;
+
+// Element constants
+const btnChangeSize = document.getElementById('btnChangeSize');
 const container = document.getElementById('container');
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const div = document.createElement('div');
-    div.className = 'grid';
 
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = 'red';
-    })
-
-    div.addEventListener('mouseout', () => {
-        div.style.backgroundColor = 'initial';
-    })
-    document.body.appendChild(div);
-    container.appendChild(div);   
+function createGrid(){
+    // For this function:
+    // 1) Check if the grid exists already and if it does, delete all the divs before creating the grid
+    // 2) Otherwise, create the grid if there's none
+    if (!div) {
+        for (let i = 0; i < gridSize * gridSize; i++) {
+            const div = document.createElement('div');
+            div.className = 'grid';
+        
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = 'red';
+            });
+        
+            div.addEventListener('mouseout', () => {
+                div.style.backgroundColor = 'initial';
+            });
+            document.body.appendChild(div);
+            container.appendChild(div);   
+        }
+    } else {
+        div.remove();
+    }  
 }
 
+createGrid();
+
+
+// Listens to the user click and runs the function
+btnChangeSize.addEventListener('click', () => {
+    gridSize = Number(prompt('Enter a new grid size:'));
+    createGrid();
+});
+
+// Testing for element existence
+let eleExist = !!document.getElementById('exists');
+console.log(eleExist);
 
 
 // const div = document.createElement('div');
