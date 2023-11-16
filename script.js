8a2ced51e4ae08849b0c7d9783247c5dda9cdc0d
Odmund Bautista <odfelip.bautista@gmail.com>
2023-11-16 10:02:46 -0600

    Create 16x16 grid
    Thinking about making it smaller, will experiment on sizes.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // Ready to script!
 const container = document.getElementById('container');
-for (let i = 0; i < 32; i ++) {
+for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
     div.className = 'grid';
-    div.textContent = 'pluh';
     document.body.appendChild(div);
     container.appendChild(div);
 }
