37d549914b80b6b0de385f7820d46b9c6851b087
Odmund Bautista <odfelip.bautista@gmail.com>
2023-11-16 11:42:43 -0600

    Finish creating grid; add button to change grid size
    Looking to transition grid styles into the JS file instead of the CSS,
    allows me more flexibility.

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 // Ready to script!
+
+// Style variables
+let gridBlockWidth = '32px';
+let gridBlockHeight = gridBlockWidth; // Should be the same as the width
+let gridBorderWidth = '2px';
+let gridSize = 16;
+let containerWidth = gridBlockWidth * gridSize;
 const container = document.getElementById('container');
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < gridSize * gridSize; i++) {
     const div = document.createElement('div');
     div.className = 'grid';
+
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'red';
+    })
+
+    div.addEventListener('mouseout', () => {
+        div.style.backgroundColor = 'initial';
+    })
     document.body.appendChild(div);
-    container.appendChild(div);
+    container.appendChild(div);   
 }
+
+
 
 // const div = document.createElement('div');
 
